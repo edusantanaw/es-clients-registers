e2befdb3d81889253332ba436d99d852d7a0908a
edusantanaw <102832370+edusantanaw@users.noreply.github.com>
2023-01-18 14:10:32 -0300

    feat: authUsecase

--- backend/src/domain/usecases/auth/auth.ts
+++ backend/src/domain/usecases/auth/auth.ts
@@ -1,17 +1,33 @@
-import { generateToken } from "../../../protocols/helper/generateToken";
-
+import { generateToken } from "../../../protocols/helper/generateToken";type User = {
+  id: string;
+  email: string;
+  password: string;
+};
 interface IUserRepository {
+  create: (email: string, password: string) => Promise<User>;
+  loadByEmail: (email: string) => Promise<User>;
+}
 
+interface IEncrypter {
+  genHash: (password: string) => Promise<string>;
+  compare: (password: string, hashedPassword: string) => Promise<boolean>;
 }
 
 export class AuthUsecase {
   constructor(
     private readonly generateToken: generateToken,
-    private readonly userRepository: IUserRepository
+    private readonly userRepository: IUserRepository,
+    private readonly encrypter: IEncrypter
   ) {}
   async auth(email: string, password: string) {
-
-    // const token = await this.generateToken.generate(username);
-    // return token;
+    const user = await this.userRepository.loadByEmail(email);
+    if (!user) throw "Usuario não encontrado!";
+    const isPasswordValid = await this.encrypter.compare(
+      password,
+      user.password
+    );
+    if (!isPasswordValid) throw "Senha invalida!";
+    const token = await this.generateToken.generate(user.id);
+    return token;
   }
 }
